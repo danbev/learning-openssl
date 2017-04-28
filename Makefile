0ec805f47396a79d997bd6d2a3f8154222f6566f
Daniel Bevenius <daniel.bevenius@gmail.com>
2017-04-28 08:39:13 +0200

    add unsecure bio read example
    This example does not use SSL but is instead intended to demonstract
    using the BIO api.

--- Makefile
+++ Makefile
@@ -4,7 +4,10 @@ OPENSSL_INCLUDE_DIR=$(OPENSSL_DIR)/include
 basic: basic.c
 	clang -O0 -g -I$(OPENSSL_INCLUDE_DIR) $< -o $@ -L$(OPENSSL_DIR) -lcrypto
 
+socket: socket.c
+	clang -O0 -g -I$(OPENSSL_INCLUDE_DIR) $< -o $@ -L$(OPENSSL_DIR) -lcrypto
+
 .PHONY: clean 
 
 clean: 
-	@rm -f basic
+	@rm -f basic socket
---
+++ socket.c
@@ -0,0 +1,52 @@
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <openssl/conf.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int arc, char *argv[]) { 
+  char* get = "GET / HTTP/1.1\x0D\x0AHost: www.google.se\x0D\x0A\x43onnection: Close\x0D\x0A\x0D\x0A";
+  char buf[1024];
+  BIO* bio;
+
+  ERR_load_crypto_strings();
+  OpenSSL_add_all_algorithms();
+  OPENSSL_no_config();
+
+  bio = BIO_new_connect("www.google.com:80");
+  if (bio == NULL) {
+    fprintf(stderr, "new_connect failed");
+    exit(1);
+  }
+
+  if (BIO_do_connect(bio) <= 0) {
+    fprintf(stderr, "do_connect failed");
+    ERR_print_errors_fp(stderr);
+    return -1;
+  }
+
+  BIO_write(bio, get, strlen(get));
+
+  for (;;) {
+    int r = BIO_read(bio, buf, 1023);
+    if (r <= 0) {
+      break;
+    }
+    buf[r] = 0;
+    fprintf(stdout, "%s", buf);
+  }
+
+  BIO_free_all(bio);
+
+  /* if you omit the next, a small leak may be left when you make use of the
+   * BIO (low level API) for e.g. base64 transformations 
+   */
+  CRYPTO_cleanup_all_ex_data();
+
+  /* Remove error strings */
+  ERR_free_strings();
+
+  return 0;
+}
+
