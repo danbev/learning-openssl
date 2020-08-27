389f9bd3f956b84b663221d9cbc86f1fb42e2c05
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-08-27 12:36:58 +0200

    src: add s_client example

---
+++ s_client.sh
@@ -0,0 +1,4 @@
+OPENSSL_DIR=../openssl_build_master
+OPENSSL_BIN=${OPENSSL_DIR}/bin
+export LD_LIBRARY_PATH=${OPENSSL_DIR}/lib
+${OPENSSL_BIN}/openssl s_client -key test.key -cert test.crt -pass "pass:test" -port 7777 
