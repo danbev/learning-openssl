88932b8619a1e701f8cbeb5757fb8595ca5f7fb4
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-08-27 14:48:30 +0200

    src: add s_client debug options

--- s_client.sh
+++ s_client.sh
@@ -4,4 +4,4 @@ export LD_LIBRARY_PATH=${OPENSSL_DIR}/lib
 #${OPENSSL_BIN}/openssl s_client -key test.key -cert test.crt -pass "pass:test" -port 7777 
 #${OPENSSL_BIN}/openssl s_client -tls1 -key test.key -cert test.crt -pass "pass:test" -port 7777 
 
-${OPENSSL_BIN}/openssl s_client -key rsa_private.pem -cert rsa_cert.crt -tls1 -port 7777 
+${OPENSSL_BIN}/openssl s_client -security_debug_verbose -debug -msg -key rsa_private.pem -cert rsa_cert.crt -tls1 -port 7777 
