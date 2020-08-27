0fd7a67d2cb5a63c2f90b9bc66ca6a73484f8f6c
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-08-27 13:59:58 +0200

    src: add debug options to s_server

--- s_server.sh
+++ s_server.sh
@@ -3,4 +3,4 @@ OPENSSL_BIN=${OPENSSL_DIR}/bin
 export LD_LIBRARY_PATH=${OPENSSL_DIR}/lib
 #${OPENSSL_BIN}/openssl s_server -key test.key -cert test.crt -pass "pass:test" -port 7777 
 
-${OPENSSL_BIN}/openssl s_server -provider legacy -provider default -key rsa_private.pem -cert rsa_cert.crt -port 7777 
+${OPENSSL_BIN}/openssl s_server -debug -msg -security_debug_verbose -provider legacy -provider default -key rsa_private.pem -cert rsa_cert.crt -port 7777 
