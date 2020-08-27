fc7c0b09e7ade5f30549b59c28d713e88611564e
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-08-27 13:39:12 +0200

    src: update s_server/s_client with useful examples

--- s_client.sh
+++ s_client.sh
@@ -1,4 +1,7 @@
 OPENSSL_DIR=../openssl_build_master
 OPENSSL_BIN=${OPENSSL_DIR}/bin
 export LD_LIBRARY_PATH=${OPENSSL_DIR}/lib
-${OPENSSL_BIN}/openssl s_client -key test.key -cert test.crt -pass "pass:test" -port 7777 
+#${OPENSSL_BIN}/openssl s_client -key test.key -cert test.crt -pass "pass:test" -port 7777 
+#${OPENSSL_BIN}/openssl s_client -tls1 -key test.key -cert test.crt -pass "pass:test" -port 7777 
+
+${OPENSSL_BIN}/openssl s_client -key rsa_private.pem -cert rsa_cert.crt -tls1 -port 7777 
--- s_server.sh
+++ s_server.sh
@@ -1,4 +1,6 @@
 OPENSSL_DIR=../openssl_build_master
 OPENSSL_BIN=${OPENSSL_DIR}/bin
 export LD_LIBRARY_PATH=${OPENSSL_DIR}/lib
-${OPENSSL_BIN}/openssl s_server -key test.key -cert test.crt -pass "pass:test" -port 7777 
+#${OPENSSL_BIN}/openssl s_server -key test.key -cert test.crt -pass "pass:test" -port 7777 
+
+${OPENSSL_BIN}/openssl s_server -provider legacy -provider default -key rsa_private.pem -cert rsa_cert.crt -port 7777 
