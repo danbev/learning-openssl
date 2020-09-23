a05d7bb6cafe628cd47097da5e2e9b713edfe292
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-09-23 13:57:41 +0200

    src: print keymgnt info

--- decoder.c
+++ decoder.c
@@ -2,6 +2,7 @@
 #include <openssl/bio.h>
 #include <openssl/err.h>
 #include <openssl/pem.h>
+#include <openssl/evp.h>
 #include <openssl/decoder.h>
 #include <stdio.h>
 #include <string.h>
@@ -12,10 +13,16 @@ void print_decoder(const char* name, void* data) {
   OSSL_DECODER* decoder = (OSSL_DECODER*) data;
   printf("name: %s\n",  name);
   printf("properties: %s\n",  OSSL_DECODER_properties(decoder));
+  printf("nr: %d\n", OSSL_DECODER_number(decoder));
 }
 
 void print_decoders(OSSL_DECODER* decoder, void* data) {
   OSSL_DECODER_names_do_all(decoder, print_decoder, decoder);
+}
+
+void print_keymgmt(EVP_KEYMGMT* keymgmt, void* arg) {
+  printf("keymgmt name: %s, nr: %d\n", EVP_KEYMGMT_get0_first_name(keymgmt),
+         EVP_KEYMGMT_number(keymgmt));
 }
 
 int main(int arc, char *argv[]) {
@@ -24,17 +31,33 @@ int main(int arc, char *argv[]) {
   provider = OSSL_PROVIDER_load(NULL, "default");
   OPENSSL_CTX* libctx = OPENSSL_CTX_new();
 
+  printf("KEY Management info:\n");
+  EVP_KEYMGMT_do_all_provided(libctx, print_keymgmt, NULL);
+
   printf("Decoder info:\n");
   OSSL_DECODER_do_all_provided(libctx, print_decoders, NULL);
 
-  BIO* bio = BIO_new_file("./rsa_private.pem", "r");
+  OSSL_DECODER* der_decoder = OSSL_DECODER_fetch(libctx, "der", NULL);
+  printf("der_decoder nr: %d\n", OSSL_DECODER_number(der_decoder));
 
-  OSSL_DECODER_CTX* decoder_ctx = OSSL_DECODER_CTX_new();;
+  OSSL_DECODER* pem_decoder = OSSL_DECODER_fetch(libctx, "pem", NULL);
+  printf("pem_decoder nr: %d\n", OSSL_DECODER_number(der_decoder));
+
+  OSSL_DECODER_CTX* decoder_ctx = OSSL_DECODER_CTX_new();
+  OSSL_DECODER_CTX_add_decoder(decoder_ctx, der_decoder);
+
+  BIO* bio = BIO_new_file("./rsa_private.pem", "r");
   int ret = OSSL_DECODER_from_bio(decoder_ctx, bio);
-  printf("decoder from bio returned: %d\n", ret);
+  if (!ret) 
+    printf("OSSL_DECODER_from_bio returned: %d\n", ret);
+
+
   ERR_print_errors_fp(stdout);
 
   BIO_free(bio);
+  OSSL_DECODER_CTX_free(decoder_ctx);
+  OSSL_DECODER_free(der_decoder);
+  OSSL_DECODER_free(pem_decoder);
   OSSL_PROVIDER_unload(provider);
   exit(EXIT_SUCCESS);
   return 0;
