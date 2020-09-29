584179e13bc2f4bc21209dc64404b4909c883138
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-09-29 06:19:47 +0200

    src: update codestyle and free up resources

--- bio_in_mem_nodejs.c
+++ bio_in_mem_nodejs.c
@@ -10,8 +10,8 @@ struct buffer {
   size_t len;
   size_t read_pos;
   size_t write_pos;
-  struct buffer* next;
-  char* data;
+  struct buffer *next;
+  char *data;
 };
 typedef struct buffer buffer;
 
@@ -20,19 +20,24 @@ struct node_bio {
   size_t length;
   size_t allocate_hint;
   int eof_return;
-  struct buffer* read_head;
-  struct buffer* write_head;
+  struct buffer *read_head;
+  struct buffer *write_head;
 };
 typedef struct node_bio node_bio;
 
-void try_move_read_head(node_bio* nbio);
-void free_empty(node_bio* nbio);
+void try_move_read_head(node_bio *nbio);
+void free_empty(node_bio *nbio);
 
-size_t node_bio_read(node_bio* nbio, char* out, size_t size) {
-  size_t bytes_read = 0;
-  size_t expected = nbio->length > size ? size : nbio->length;
-  size_t offset = 0;
-  size_t left = size;
+size_t node_bio_read(node_bio *nbio, char *out, size_t size) {
+  size_t bytes_read;
+  size_t expected;
+  size_t offset;
+  size_t left;
+
+  bytes_read = 0;
+  expected = nbio->length > size ? size : nbio->length;
+  offset = 0;
+  left = size;
 
   while (bytes_read < expected) {
     size_t avail = nbio->read_head->write_pos - nbio->read_head->read_pos;
@@ -60,26 +65,26 @@ size_t node_bio_read(node_bio* nbio, char* out, size_t size) {
   return bytes_read;
 }
 
-void free_empty(node_bio* nbio) {
+void free_empty(node_bio *nbio) {
   if (nbio->write_head == NULL)
     return;
-  buffer* child = nbio->write_head->next;
+  buffer *child = nbio->write_head->next;
   if (child == nbio->write_head || child == nbio->read_head)
     return;
-  buffer* cur = child->next;
+  buffer *cur = child->next;
   if (cur == nbio->write_head || cur == nbio->read_head)
     return;
 
-  buffer* prev = child;
+  buffer *prev = child;
   while (cur != nbio->read_head) {
-    buffer* next = cur->next;
+    buffer *next = cur->next;
     free(cur->data);
     cur = next;
   }
   prev->next = cur;
 }
 
-void try_move_read_head(node_bio* nbio) {
+void try_move_read_head(node_bio *nbio) {
   // `read_pos_` and `write_pos_` means the position of the reader and writer
   // inside the buffer, respectively. When they're equal - its safe to reset
   // them, because both reader and writer will continue doing their stuff
@@ -98,11 +103,11 @@ void try_move_read_head(node_bio* nbio) {
 }
 
 
-int read(BIO* bio, char* out, int len) {
+int read(BIO *bio, char *out, int len) {
   printf("%s read\n", BIO_method_name(bio));
 
   // Get buffer from bio.
-  node_bio* nbio = BIO_get_data(bio);
+  node_bio *nbio = BIO_get_data(bio);
   int bytes = node_bio_read(nbio, out, len);
   if (bytes == 0) {
     bytes = nbio->eof_return;
@@ -113,9 +118,11 @@ int read(BIO* bio, char* out, int len) {
   return bytes;
 }
 
-void try_allocate_for_write(node_bio* nbio, size_t hint) {
-  buffer* w = nbio->write_head;
-  buffer* r = nbio->read_head;
+void try_allocate_for_write(node_bio *nbio, size_t hint) {
+  buffer *w;
+  buffer *r;
+  w = nbio->write_head;
+  r = nbio->read_head;
   // If write head is full, next buffer is either read head or not empty.
   if (w == NULL ||
       (w->write_pos == w->len &&
@@ -131,7 +138,7 @@ void try_allocate_for_write(node_bio* nbio, size_t hint) {
     }
 
     //buffer* next = new Buffer(env_, len);
-    buffer* next = (buffer*) malloc(sizeof(buffer));
+    buffer *next = (buffer*) malloc(sizeof(buffer));
     next->len = len;
     next->read_pos = 0;
     next->write_pos = 0;
@@ -149,9 +156,11 @@ void try_allocate_for_write(node_bio* nbio, size_t hint) {
   }
 }
 
-void node_bio_write(node_bio* nbio, const char* data, size_t size) {
-  size_t offset = 0;
-  size_t left = size;
+void node_bio_write(node_bio *nbio, const char *data, size_t size) {
+  size_t offset;
+  size_t left;
+  offset = 0;
+  left = size;
 
   // Allocate initial buffer if the ring is empty
   try_allocate_for_write(nbio, left);
@@ -190,17 +199,17 @@ void node_bio_write(node_bio* nbio, const char* data, size_t size) {
   //CHECK_EQ(left, 0);
 }
 
-int write(BIO* bio, const char* data, int len) {
+int write(BIO *bio, const char *data, int len) {
   BIO_clear_retry_flags(bio);
 
-  node_bio* nbio = BIO_get_data(bio);
+  node_bio *nbio = BIO_get_data(bio);
   node_bio_write(nbio, data, len);
 
   return len;
 }
 
 
-void reset(node_bio* nbio) {
+void reset(node_bio *nbio) {
   if (nbio->read_head == NULL)
     return;
 
@@ -214,8 +223,8 @@ void reset(node_bio* nbio) {
   nbio->write_head = nbio->read_head;
 }
 
-long ctrl(BIO* bio, int cmd, long larg, void* parg) {
-  node_bio* nbio;
+long ctrl(BIO *bio, int cmd, long larg, void *parg) {
+  node_bio *nbio;
   long ret;
   nbio = BIO_get_data(bio);
   ret = 1;
@@ -268,11 +277,27 @@ long ctrl(BIO* bio, int cmd, long larg, void* parg) {
   return ret;
 }
 
+int node_bio_free(BIO *bio) {
+  if (bio == NULL)
+    return 0;
+
+  if (BIO_get_shutdown(bio)) {
+    if (BIO_get_init(bio) && BIO_get_data(bio) != NULL) {
+      node_bio* nbio = BIO_get_data(bio);
+      free_empty(nbio);
+      BIO_set_data(bio, NULL);
+    }
+  }
+
+  return 1;
+}
+
+
 int main(int arc, char *argv[]) {
   printf("BIO in memory Node.js PEM_read_bio_PrivateKey issue\n");
-  BIO_METHOD* method;
-  BIO* file_bio;
-  BIO* in_mem_bio;
+  BIO_METHOD *method;
+  BIO *file_bio;
+  BIO *in_mem_bio;
 
   file_bio = BIO_new_file("./rsa_private.pem", "r");
   unsigned char data[4096];
@@ -283,6 +308,7 @@ int main(int arc, char *argv[]) {
   BIO_meth_set_read(method, read);
   BIO_meth_set_write(method, write);
   BIO_meth_set_ctrl(method, ctrl);
+  BIO_meth_set_destroy(method, node_bio_free);
 
   in_mem_bio = BIO_new(method);
   char buf_data[length];
@@ -300,5 +326,7 @@ int main(int arc, char *argv[]) {
     printf("cound not read private key!\n");
 
   BIO_free(in_mem_bio);
+  BIO_free(file_bio);
+  EVP_PKEY_free(key);
   exit(EXIT_SUCCESS);
 }
