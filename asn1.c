45e4837684c6bc38fbf25d5ee2f5b01dd125afe1
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-10-01 09:22:14 +0200

    src: add example of encoding/decoding a structure

--- asn1.c
+++ asn1.c
@@ -1,11 +1,12 @@
 #include <openssl/asn1.h>
 #include <openssl/asn1t.h>
+#include <openssl/err.h>
 
-struct something {
-  int age;
+struct something_st {
+  int32_t age;
 };
 
-typedef struct something something;
+typedef struct something_st something;
 
 static int something_cb(int operation, ASN1_VALUE **pval, const ASN1_ITEM *it,
                         void *exarg) {
@@ -14,14 +15,28 @@ static int something_cb(int operation, ASN1_VALUE **pval, const ASN1_ITEM *it,
 }
 
 ASN1_SEQUENCE_cb(something, something_cb) = {
-  ASN1_SIMPLE(something, age, ASN1_INTEGER)
+  ASN1_EMBED(something, age, INT32)
 } ASN1_SEQUENCE_END_cb(something, something)
 
 IMPLEMENT_ASN1_FUNCTIONS(something)
 
 int main(int argc, char** argv) {
   printf("OpenSSL asn1 example\n");
-  const ASN1_ITEM* item = something_it();
+
+  const something s = { 88 };
+  unsigned char* out = NULL;
+  // internal C structure to DER binary format
+  int ret = i2d_something(&s, &out);
+  if (ret) {
+    printf("Encoded something {%d} into: %s\n", s.age, out);
+  }
+
+  const unsigned char* encoded_something = out;
+  // DER binary format to internal C structure
+  something* decoded = d2i_something(NULL, &encoded_something, ret);
+  printf("Decoded something {%d}\n", decoded->age);
+
+  ERR_print_errors_fp(stdout);
 
   return 0;
 }
