bdd314bfc3706ec93285e18e0eb7779a8f858ff9
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-10-05 18:30:30 +0200

    src: add example of ERR functions usages

--- err.c
+++ err.c
@@ -10,7 +10,7 @@ int main(int arc, char *argv[]) {
   OSSL_PROVIDER* provider;
   provider = OSSL_PROVIDER_load(NULL, "default");
 
-  int example_lib = 1288;
+  int example_lib = ERR_get_next_error_library();
   int reason_1 = 1;
   int reason_2 = 2;
 
@@ -21,14 +21,18 @@ int main(int arc, char *argv[]) {
   // Register the first error
   const ERR_STRING_DATA str_reason = { ERR_PACK(example_lib, 0, reason_1), "something bad happened 1"};
   ERR_load_strings_const(&str_reason);
+  int lib_nr = ERR_GET_LIB(str_reason.error);
+  printf("lib_nr: %d\n", lib_nr);
 
   // Register the second error
   const ERR_STRING_DATA str_reason_1= { ERR_PACK(example_lib, 0, reason_2), "something bad happened 2"};
   ERR_load_strings_const(&str_reason_1);
 
   ERR_raise(example_lib, reason_1);
-  ERR_raise(example_lib, reason_2);
+  int error = ERR_get_error();
+  printf("%s:%s\n", ERR_lib_error_string(error), ERR_reason_error_string(error));
 
+  ERR_raise_data(example_lib, reason_2, "details go here...");
   ERR_print_errors_fp(stdout);
 
   OSSL_PROVIDER_unload(provider);
