6d639d23fcab8b623df917571b8366a67e2b1cdf
Daniel Bevenius <daniel.bevenius@gmail.com>
2020-11-05 14:25:56 +0100

    src: add thread to ec-keygen example
    This commit adds thread to simulate the issue that I'm currently looking
    into.

--- ec-keygen.c
+++ ec-keygen.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
 void error_and_exit(const char* msg) {
   printf("%s\n", msg);
@@ -13,13 +14,32 @@ void error_and_exit(const char* msg) {
   int err = ERR_get_error();
   ERR_error_string_n(err, buf, sizeof(buf));
   printf("errno: %d, %s\n", err, buf);
-  exit(EXIT_FAILURE);
+}
+
+void* get_ec_key(void* args) {
+  EVP_PKEY* pkey = (EVP_PKEY*) args;
+  printf("[%u] get_ec_keys: pkey: %p\n", pthread_self(), pkey);
+  EC_KEY* ec_key = EVP_PKEY_get0_EC_KEY(pkey);
+  printf("[%u] get_ec_keys: ec_key: %p\n", pthread_self(), ec_key);
+  pthread_exit(NULL);
+}
+
+void* get_pkcs8(void* args) {
+  EVP_PKEY* pkey = (EVP_PKEY*) args;
+  printf("[%u] get_pkcs8: pkey: %p\n", pthread_self(), pkey);
+
+  PKCS8_PRIV_KEY_INFO* pkcs8 = EVP_PKEY2PKCS8(pkey);
+  BIO* pkcs8_bio = BIO_new(BIO_s_mem());
+  if  (i2d_PKCS8_PRIV_KEY_INFO_bio(pkcs8_bio, pkcs8) <= 0) {
+    error_and_exit("Could not convert to PKCS8 format");
+  }
+  pthread_exit(NULL);
 }
 
 int main(int arc, char *argv[]) {
   //const char* curve_name = "secp384r1";
   const char* curve_name = "P-384";
-  printf("Elliptic Curve kegen (%s) example\n", curve_name);
+  printf("Elliptic Curve keygen (%s) example \n", curve_name);
   int curve_nid = EC_curve_nist2nid(curve_name);
   if (curve_nid == NID_undef) {
     // try converting the shortname (sn) to nid (numberic id)
@@ -28,7 +48,6 @@ int main(int arc, char *argv[]) {
   printf("curve_nid of %s: %d\n", curve_name, curve_nid);
 
   // The last argument is the ENGINE*.
-  // 
   EVP_PKEY_CTX* ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_EC, NULL);
   // The following function is for generating parameters
   if (EVP_PKEY_paramgen_init(ctx) <= 0) {
@@ -39,6 +58,7 @@ int main(int arc, char *argv[]) {
   if (EVP_PKEY_CTX_set_ec_paramgen_curve_nid(ctx, curve_nid) <= 0) {
     error_and_exit("Could not set the param curve nid");
   }
+
   // Set the parameter encoding which can be either OPENSSL_EC_EXPLICIT_CURVE
   // or OPENSSL_EC_NAMED_CURVE. The default for OpenSSL 3.x is named curve
   printf("Set ec_param_encoding to %d\n", OPENSSL_EC_NAMED_CURVE);
@@ -65,33 +85,11 @@ int main(int arc, char *argv[]) {
     error_and_exit("Could not generate the private key");
   }
 
-  // The '1' indicates that the ref count will be decremented so it must be
-  // freed by us. Use EVP_PKEY_get0_EC_KEY to avoid this.
-  EC_KEY* ec_key = EVP_PKEY_get1_EC_KEY(pkey);
-  const BIGNUM* b = EC_KEY_get0_private_key(ec_key);
-  BIO* out = BIO_new(BIO_s_mem());
-
-  int len = PEM_write_bio_ECPrivateKey(out, ec_key, NULL, NULL, 0, NULL, NULL);
-  if (len <= 0) {
-    error_and_exit("Could not write the private key");
-  }
-  BUF_MEM* bptr;
-  BIO_get_mem_ptr(out, &bptr);
-  printf("%s\n", bptr->data);
-
-  BIO* pub_out = BIO_new(BIO_s_mem());
-  len = PEM_write_bio_EC_PUBKEY(pub_out, ec_key);
-  if (len <= 0) {
-    error_and_exit("Could not write the private key");
-  }
-  BIO_get_mem_ptr(pub_out, &bptr);
-  printf("%s\n", bptr->data);
-
-  const EC_GROUP* group = EC_KEY_get0_group(ec_key);
-  int order = EC_GROUP_order_bits(group);
-  printf("Group order: %d\n", order);
-
-  const EC_POINT* generator = EC_GROUP_get0_generator(group);
+  printf("created EVP_PKEY, now create threads and pass reference as args\n");
+  pthread_t get_pkcs8_t;
+  pthread_t get_ec_key_t;
+  pthread_create(&get_pkcs8_t, NULL, get_pkcs8, pkey);
+  pthread_create(&get_ec_key_t, NULL, get_ec_key, pkey);
 
   EVP_PKEY_CTX_free(ctx);
   exit(EXIT_SUCCESS);
