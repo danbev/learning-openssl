c37666299b4e37658ac5d5a677af41b0a8f48c10
Daniel Bevenius <daniel.bevenius@gmail.com>
2021-04-27 12:41:52 +0200

    src: fix wrong-tag2.c to simulate issue properly

--- wrong-tag2.c
+++ wrong-tag2.c
@@ -15,13 +15,27 @@ void error_and_exit(const char* msg) {
   exit(EXIT_FAILURE);
 }
 
+static int passwd_callback(char* buf, int size, int rwflag, void* u) {
+  const char* passphrase = (char*) u;
+  if (passphrase != NULL) {
+    size_t buflen = (size_t) size;
+    size_t len = strlen(passphrase);
+    if (buflen < len)
+      return -1;
+    memcpy(buf, passphrase, len);
+    return len;
+  }
+  return -1;
+} 
+
 int main(int arc, char *argv[]) {
   printf("asn1 wrong tag issue (#2)\n");
-  BIO *file_bio;
-  BIO *key_bio;
+  BIO* file_bio;
+  BIO* key_bio;
   EVP_PKEY* pkey = NULL;
   PKCS8_PRIV_KEY_INFO *p8inf = NULL;
 
+  // Private key in pem format (DER in base64 format)
   file_bio = BIO_new_file("./rsa_private2.pem", "r");
   
   unsigned char key[4096];
@@ -30,17 +44,31 @@ int main(int arc, char *argv[]) {
   printf("key: %s\n", key);
 
   key_bio = BIO_new_mem_buf(key, key_len);
+  pkey = PEM_read_bio_PrivateKey(key_bio, NULL, passwd_callback, ""); 
 
-  p8inf = d2i_PKCS8_PRIV_KEY_INFO_bio(key_bio, NULL);
+  BIO_free(file_bio);
+
+  key_bio = BIO_new_mem_buf(pkey, key_len);
+
+  BIO* b = BIO_new(BIO_s_mem());
+  int err = i2d_PKCS8PrivateKey_bio(b, pkey, NULL, NULL, 0, NULL, NULL);
+
+  BUF_MEM* bptr;
+  BIO_get_mem_ptr(b, &bptr);
+
+  p8inf = d2i_PKCS8_PRIV_KEY_INFO_bio(b, NULL);
   if (p8inf == NULL) {
     error_and_exit("check errors");
   }
 
   pkey = EVP_PKCS82PKEY(p8inf);
+  if (pkey == NULL) {
+    error_and_exit("check errors");
+  }
 
-  BIO_free(file_bio);
   BIO_free(key_bio);
   EVP_PKEY_free(pkey);
+  error_and_exit("check errors");
 
   exit(EXIT_SUCCESS);
 }
