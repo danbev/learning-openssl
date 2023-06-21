ec96314a974520bff2983efe55447ed1e2aacbc6
Enji Cooper <yaneurabeya@gmail.com>
2023-06-20 21:07:46 -0700

    Fix a -Wformat issue
    Use the correct format string with printf(3).
    
    Signed-off-by: Enji Cooper <yaneurabeya@gmail.com>

--- bignum.c
+++ bignum.c
@@ -7,7 +7,7 @@ int main(int argc, char** argv) {
   printf("BIGNUM example\n");
   BIGNUM* nr = BN_new();
   BN_set_word(nr, 3);
-  printf("nr: %d\n", BN_get_word(nr));
+  printf("nr: %lu\n", BN_get_word(nr));
 
   BIGNUM* nr2 = BN_new();
   BN_set_word(nr2, 1);
