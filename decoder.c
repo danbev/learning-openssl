14e3d513e0a50db637bb0321495078fa9b0441b7
Enji Cooper <yaneurabeya@gmail.com>
2023-06-20 21:37:10 -0700

    decoder: unbreak the build on 3.0.9
    - Comment out internal methods.
    - Use documented accessors.
    - Use documented structures.
    
    Signed-off-by: Enji Cooper <yaneurabeya@gmail.com>

--- decoder.c
+++ decoder.c
@@ -12,8 +12,10 @@
 void print_decoder(const char* name, void* data) {
   OSSL_DECODER* decoder = (OSSL_DECODER*) data;
   printf("name: %s\n",  name);
-  printf("properties: %s\n",  OSSL_DECODER_properties(decoder));
+  printf("properties: %s\n",  OSSL_DECODER_get0_properties(decoder));
+#if 0
   printf("nr: %d\n", OSSL_DECODER_number(decoder));
+#endif
 }
 
 void print_decoders(OSSL_DECODER* decoder, void* data) {
@@ -21,15 +23,19 @@ void print_decoders(OSSL_DECODER* decoder, void* data) {
 }
 
 void print_keymgmt(EVP_KEYMGMT* keymgmt, void* arg) {
-  printf("keymgmt name: %s, nr: %d\n", EVP_KEYMGMT_get0_first_name(keymgmt),
+#if 0
+  printf("keymgmt name: %s, nr: %d\n", EVP_KEYMGMT_get0_name(keymgmt),
          EVP_KEYMGMT_number(keymgmt));
+#else
+  printf("keymgmt name: %s\n", EVP_KEYMGMT_get0_name(keymgmt));
+#endif
 }
 
 int main(int arc, char *argv[]) {
   printf("OpenSSL decoder example\n");
   OSSL_PROVIDER* provider;
   provider = OSSL_PROVIDER_load(NULL, "default");
-  OPENSSL_CTX* libctx = OPENSSL_CTX_new();
+  OSSL_LIB_CTX* libctx = OSSL_LIB_CTX_new();
 
   printf("KEY Management info:\n");
   EVP_KEYMGMT_do_all_provided(libctx, print_keymgmt, NULL);
@@ -38,11 +44,15 @@ int main(int arc, char *argv[]) {
   OSSL_DECODER_do_all_provided(libctx, print_decoders, NULL);
 
   OSSL_DECODER* der_decoder = OSSL_DECODER_fetch(libctx, "der", NULL);
+#if 0
   printf("der_decoder nr: %d\n", OSSL_DECODER_number(der_decoder));
+#endif
 
   OSSL_DECODER* pem_decoder = OSSL_DECODER_fetch(libctx, "RSA",
       "provider=default,fips=yes,input=pem");
+#if 0
   printf("pem_decoder nr: %d\n", OSSL_DECODER_number(pem_decoder));
+#endif
   OSSL_DECODER_names_do_all(pem_decoder, print_decoder, pem_decoder);
 
   OSSL_DECODER_CTX* decoder_ctx = OSSL_DECODER_CTX_new();
